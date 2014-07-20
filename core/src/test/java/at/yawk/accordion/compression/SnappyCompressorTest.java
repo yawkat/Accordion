af2215356e3e3732dc5606c1a0b2f1f25f0ab09f
Jonas Konrad <me@yawk.at>
2014-07-21 00:05:08 +0200

    Make snappy compression test a bit more extensive

--- core/src/test/java/at/yawk/accordion/compression/SnappyCompressorTest.java
+++ core/src/test/java/at/yawk/accordion/compression/SnappyCompressorTest.java
@@ -4,13 +4,25 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Random;
+
 import static org.junit.Assert.assertEquals;
 
 public class SnappyCompressorTest {
     @Test
     public void testCompress() {
+        Random random = new Random(0);
+        for (int i = 0; i < 10000; i++) {
+            byte[] data = new byte[random.nextInt(10000)];
+            for (int j = 0; j < data.length; j++) data[j] = (byte) random.nextInt(256);
+            testCompressDecompress(data);
+        }
+    }
+
+    private void testCompressDecompress(byte[] payload) {
         ByteBuf unc = Unpooled.buffer();
-        unc.writeBytes(new byte[]{ 't', 'e', 's', 't' });
+        unc.writeBytes(payload);
 
         ByteBuf comp = SnappyCompressor.getInstance().encode(unc);
         ByteBuf unc2 = SnappyCompressor.getInstance().decode(comp);
